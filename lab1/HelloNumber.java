0a0dfe323e609271c513e0b3c102c03f178fdce9
liutianle20 <lltkobe824@gmail.com>
2023-12-01 00:57:43 +0800

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,11 @@
+public class HelloNumber {
+    public static void main(String[] args){
+        int x = 0;
+        int count = 0;
+        while (x < 10) {
+            count = count + x;
+            System.out.println(count + " ");
+            x = x + 1;
+        }
+    }
+}
